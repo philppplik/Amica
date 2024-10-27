9ac82dce95004bccd6b24f786c26edfa44e02a7a
Philipp <92173046+philppplik@users.noreply.github.com>
2024-10-27 11:16:39 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        amica: {
+          light: '#4285f4',
+          dark: '#3367d6',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
